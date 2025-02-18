02681b714bbef31182e82e6c23376278bd1779ee
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-02-17 22:09:38 -0500

    report raw pixel counts without clustering

--- app.py
+++ app.py
@@ -2,21 +2,36 @@ import gradio as gr
 
 from dominant_color import get_dominant_colors
 
-NUM_COLORS = 4
-NUM_OUTS = 2 * NUM_COLORS + NUM_COLORS
+NUM_CNT = 8
+NUM_HLS = 4
+NUM_RAW = 12
 
-out_colors = [gr.ColorPicker(visible=False) for _ in range(NUM_OUTS)]
-out_pcts = [gr.Textbox(visible=False) for _ in range(NUM_OUTS)]
+cnt_colors = [gr.ColorPicker(visible=False) for _ in range(NUM_CNT)]
+cnt_pcts = [gr.Textbox(visible=False) for _ in range(NUM_CNT)]
+
+hls_colors = [gr.ColorPicker(visible=False) for _ in range(NUM_HLS)]
+hls_pcts = [gr.Textbox(visible=False) for _ in range(NUM_HLS)]
+
+raw_colors = [gr.ColorPicker(visible=False) for _ in range(NUM_RAW)]
+raw_pcts = [gr.Textbox(visible=False) for _ in range(NUM_RAW)]
 
 def dom_col(img_in):
-  palette_cnt, palette_hls, palette_pcts, hex_pcts, img_out = get_dominant_colors(img_in, k=NUM_COLORS)
-  palette_hex = palette_cnt + palette_hls
+  palette_cnt, palette_hls, palette_pcts, hex_pcts, img_out = get_dominant_colors(img_in, k=NUM_HLS)
 
-  def get_pct_md(cp):
-    return f"### {round(palette_pcts.get(cp, 0)*100, 2)}%"
+  def get_pct_md(pct):
+    return f"### {round(pct*100, 2)}%"
 
-  # TODO: use hex_pcts for something
-  return [gr.ColorPicker(h) for h in palette_hex] + [gr.Textbox(get_pct_md(h)) for h in palette_hex] + [img_out]
+  def get_hex_pct_md(cp):
+    return get_pct_md(palette_pcts.get(cp, 0))
+
+  cnt_colors = [gr.ColorPicker(h) for h in palette_cnt]
+  cnt_pcts = [gr.Textbox(get_hex_pct_md(h)) for h in palette_cnt]
+  hls_colors = [gr.ColorPicker(h) for h in palette_hls]
+  hls_pcts = [gr.Textbox(get_hex_pct_md(h)) for h in palette_hls]
+  raw_colors = [gr.ColorPicker(hp[0]) for hp in hex_pcts[:NUM_RAW]]
+  raw_pcts = [gr.Textbox(get_pct_md(hp[1])) for hp in hex_pcts[:NUM_RAW]]
+
+  return cnt_colors + cnt_pcts + hls_colors + hls_pcts + raw_colors + raw_pcts + [img_out]
 
 def get_color(cp):
   return cp
@@ -34,7 +49,10 @@ with gr.Blocks() as demo:
       gr.Interface(
         dom_col,
         inputs=gr.Image(type="pil"),
-        outputs=[*out_colors, *out_pcts, gr.Image(type="pil", label="img_out")],
+        outputs=[*cnt_colors, *cnt_pcts,
+                 *hls_colors, *hls_pcts,
+                 *raw_colors, *raw_pcts,
+                 gr.Image(type="pil", label="img_out")],
         cache_examples=True,
         examples=[["./imgs/03.webp"], ["./imgs/11.jpg"]],
         allow_flagging="never",
@@ -44,19 +62,27 @@ with gr.Blocks() as demo:
     with gr.Column(scale=0, variant="default"):
       with gr.Column(scale=0, variant="panel"):
         gr.Markdown("# By Count")
-        for i,o in enumerate(out_colors[:2*NUM_COLORS]):
+        for i,o in enumerate(cnt_colors):
           with gr.Row():
             gr.ColorPicker(get_color, inputs=[o], show_label=False, scale=0, container=False)
             gr.Markdown(get_color_md, inputs=[o], show_label=False)
-            gr.Markdown(get_md, inputs=[out_pcts[i]], show_label=False)
+            gr.Markdown(get_md, inputs=[cnt_pcts[i]], show_label=False)
 
       with gr.Column(scale=0, variant="panel"):
         gr.Markdown("# By HLS")
-        for i,o in enumerate(out_colors[-NUM_COLORS:]):
+        for i,o in enumerate(hls_colors):
           with gr.Row():
             gr.ColorPicker(get_color, inputs=[o], show_label=False, scale=0, container=False)
             gr.Markdown(get_color_md, inputs=[o], show_label=False)
-            gr.Markdown(get_md, inputs=[out_pcts[-NUM_COLORS+i]], show_label=False)
+            gr.Markdown(get_md, inputs=[hls_pcts[i]], show_label=False)
+
+      with gr.Column(scale=0, variant="panel"):
+        gr.Markdown("# By Raw Count")
+        for i,o in enumerate(raw_colors):
+          with gr.Row():
+            gr.ColorPicker(get_color, inputs=[o], show_label=False, scale=0, container=False)
+            gr.Markdown(get_color_md, inputs=[o], show_label=False)
+            gr.Markdown(get_md, inputs=[raw_pcts[i]], show_label=False)
 
 
 if __name__ == "__main__":
